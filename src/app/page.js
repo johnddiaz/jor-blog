1e14b49e64c342d1f5971144a2a785860c1fbe37
johnddiaz <jchubbs95@gmail.com>
2023-09-16 17:02:46 -0600

    forgot stuff

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import { getBlogPostList } from '@/helpers/file-helpers';
 
 async function Home() {
   const posts = await getBlogPostList();
-  console.log(posts);
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
@@ -15,6 +15,7 @@ async function Home() {
       {posts.map((post) => {
         return (
           <BlogSummaryCard
+            key={post.slug}
             slug={post.slug}
             title={post.title}
             abstract={post.abstract}
