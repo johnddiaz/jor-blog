85ba1e4d7c0d7ae127305262573b037cacf84e9a
johnddiaz <jchubbs95@gmail.com>
2023-09-16 20:37:31 -0600

    exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,12 +6,25 @@ import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+export async function generateMetadata({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+
+  return {
+    title: post.frontmatter.title,
+    description: post.frontmatter.abstract,
+  };
+}
+
 async function BlogPost({ params }) {
   const post = await loadBlogPost(params.postSlug);
 
   return (
     <article className={styles.wrapper}>
-      <BlogHero title={post.frontmatter.title} publishedOn={post.frontmatter.publishedOn} />
+      <BlogHero
+        abstract={post.frontmatter.abstract}
+        title={post.frontmatter.title}
+        publishedOn={post.frontmatter.publishedOn}
+      />
       <div className={styles.page}>
         <MDXRemote source={post.content} />
       </div>
